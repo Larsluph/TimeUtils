74d7b6bdc27a4484c1ad24cdee11cf0e1eda85ae
Larsluph <remarso59@sfr.fr>
2022-04-17 08:43:54 +0200

    fix incorrect field detection

--- app/src/main/java/com/larsluph/timeutils/calc/CalcActivity.java
+++ app/src/main/java/com/larsluph/timeutils/calc/CalcActivity.java
@@ -2,6 +2,7 @@ package com.larsluph.timeutils.calc;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -20,90 +21,155 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalcActivity extends AppCompatActivity {
-    private final Pattern pDays = Pattern.compile("(\\d+)d");
-    private final Pattern pHours = Pattern.compile("(\\d+)h");
-    private final Pattern pMinutes = Pattern.compile("(\\d+)m");
-    private final Pattern pSeconds = Pattern.compile("(\\d+)s");
+    /**
+     * Pattern to match valid date.
+     * Matches:
+     * - DD-MM
+     * - DD/MM
+     * - DD-MM-YYYY
+     * - DD/MM-YYYY
+     * - DD-MM/YYYY
+     * - DD/MM/YYYY
+     */
+    private static final Pattern pDate = Pattern.compile("^(\\d{1,2})[/-](\\d{1,2})(?:[/-](\\d{4}))?$");
+
+    /**
+     * Pattern to match valid time
+     * Matches:
+     * - hh:mm
+     * - hh:mm:ss
+     */
+    private static final Pattern pTime = Pattern.compile("^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])(?::([0-5]?[0-9]))?$");
+
+    private static final Pattern pDays = Pattern.compile("(-?\\d+)d");
+    private static final Pattern pHours = Pattern.compile("(-?\\d+)h");
+    private static final Pattern pMinutes = Pattern.compile("(-?\\d+)m");
+    private static final Pattern pSeconds = Pattern.compile("(-?\\d+)s");
+
+    private EditText textDate;
+    private EditText textTime;
+    private EditText textDelta;
+    private TextView result;
+
+    private boolean dispDate;
+    private boolean dispYear;
+    private boolean dispTime;
+    private boolean dispSeconds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
 
-        ((EditText)findViewById(R.id.deltaDateTime)).setOnEditorActionListener((v, id, e) -> {
+        textDate = findViewById(R.id.baseDate);
+        textTime = findViewById(R.id.baseTime);
+        textDelta = findViewById(R.id.deltaDateTime);
+        result = findViewById(R.id.calcResult);
+
+        textDelta.setOnEditorActionListener((v, id, e) -> {
             calcResult();
             return true;
         });
         findViewById(R.id.buttonCalc).setOnClickListener(v -> calcResult());
     }
 
-    private void calcResult() {
+    /**
+     * Hide keyboard if {token} is focused
+     * @param token Token
+     */
+    private void hideKeyboard(IBinder token) {
         InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(findViewById(R.id.deltaDateTime).getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(token, 0);
+    }
 
-        // fetch fields from layout
-        final EditText textDate = findViewById(R.id.baseDate);
-        final EditText textTime = findViewById(R.id.baseTime);
-        final EditText deltaDateTime = findViewById(R.id.deltaDateTime);
-        final TextView result = findViewById(R.id.calcResult);
+    /**
+     * Takes a string as input and check if it's a valid date
+     * @param input input string
+     */
+    public static boolean validateDate(String input) {
+        Matcher date = pDate.matcher(input);
+        return date.matches();
+    }
 
-        boolean dispDate = true;
-        boolean dispYear = false;
-        boolean dispTime = true;
-        boolean dispSeconds = false;
+    /**
+     * Takes a string as input and check if it's a valid time
+     * @param input input string
+     */
+    public static boolean validateTime(String input) {
+        Matcher time = pTime.matcher(input);
+        return time.matches();
+    }
 
-        // set default datetime
-        LocalDate baseDate = LocalDate.now();
-        LocalTime baseTime = LocalTime.now();
-
-        // base date detection
-        try {
-            String[] matchs = textDate.getText().toString()
-                                                .replace("/", "-")
-                                                .split("-");
+    /**
+     * Takes a string as input and converts it to a LocalDate object
+     * @param input input string
+     */
+    public LocalDate detectDate(String input) {
+        LocalDate date = LocalDate.now();
+        if (input.isEmpty()) {
+            // no input, return current date
+            dispDate = false;
+            return date;
+        } else if (validateDate(input)) {
+            // input is valid date
+            String[] matchs = input.replace("/", "-").split("-");
             switch (matchs.length) {
                 case 3:
-                    baseDate = baseDate.withYear(Integer.parseInt(matchs[2]));
+                    date = date.withYear(Integer.parseInt(matchs[2]));
                     dispYear = true;
                 case 2:
-                    baseDate = baseDate.withMonth(Integer.parseInt(matchs[1]))
-                                       .withDayOfMonth(Integer.parseInt(matchs[0]));
+                    date = date.withMonth(Integer.parseInt(matchs[1]))
+                            .withDayOfMonth(Integer.parseInt(matchs[0]));
                     break;
                 default:
                     throw new NumberFormatException("Invalid Date Format");
             }
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            Log.d("date parsing", "invalid date");
-            dispDate = false;
+        } else {
+            // input is invalid date, display field
+            Log.w("date parsing", "invalid date");
+            dispYear = true;
         }
+        return date;
+    }
 
-        // base time detection
-        try {
-            String[] matchs = textTime.getText().toString().split(":");
+    /**
+     * Takes a string as input and converts it to a LocalTime object
+     * @param input input string
+     */
+    public LocalTime detectTime(String input) {
+        LocalTime time = LocalTime.now();
+        if (input.isEmpty()) {
+            // no input, return current time
+            dispTime = false;
+            return time;
+        } else if (validateTime(input)) {
+            // input is valid time
+            String[] matchs = input.split(":");
             switch (matchs.length) {
                 case 3:
-                    baseTime = baseTime.withSecond(Integer.parseInt(matchs[2]));
+                    time = time.withSecond(Integer.parseInt(matchs[2]));
                     dispSeconds = true;
                 case 2:
-                    baseTime = baseTime.withHour(Integer.parseInt(matchs[0]))
-                                       .withMinute(Integer.parseInt(matchs[1]));
+                    time = time.withHour(Integer.parseInt(matchs[0]))
+                            .withMinute(Integer.parseInt(matchs[1]));
                     break;
                 default:
                     throw new NumberFormatException("Invalid Time Format");
             }
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            Log.d("time parsing", "invalid time");
-            dispTime = false;
+        } else {
+            // input is invalid time, display field
+            Log.w("time parsing", "invalid time");
+            dispSeconds = true;
         }
+        return time;
+    }
 
-        // gen dt from values
-        LocalDateTime baseDateTime = LocalDateTime.of(baseDate, baseTime);
-
-        // delta datetime detection
-        final String input = deltaDateTime.getText().toString();
-
-        // regex matching
+    /**
+     * Takes a string as input and converts it to a TimeSpan object
+     * @param input input string
+     */
+    public TimeSpan detectDelta(String input) {
+        // apply regex
         Matcher mDays = pDays.matcher(input);
         Matcher mHours = pHours.matcher(input);
         Matcher mMinutes = pMinutes.matcher(input);
@@ -129,13 +195,28 @@ public class CalcActivity extends AppCompatActivity {
             seconds += Integer.parseInt(Objects.requireNonNull(mSeconds.group(1)));
         }
 
-        // compute final dt
-        LocalDateTime finalDateTime = baseDateTime.plusDays(days)
-            .plusHours(hours)
-            .plusMinutes(minutes)
-            .plusSeconds(seconds);
+        // return formatted delta
+        return new TimeSpan(days, hours, minutes, seconds);
+    }
 
-        // render with correct format
+    /**
+     * Add a TimeSpan to a LocalDateTime
+     * @param base base datetime
+     * @param delta delta to add to base
+     */
+    public LocalDateTime processDelta(LocalDateTime base, TimeSpan delta) {
+        return base
+                .plusDays(delta.days)
+                .plusHours(delta.hours)
+                .plusMinutes(delta.minutes)
+                .plusSeconds(delta.seconds);
+    }
+
+    /**
+     * Render processed delta to result text box
+     * @param finalDateTime processed delta to display
+     */
+    private void renderResult(LocalDateTime finalDateTime) {
         result.setTextColor(getResources().getColor(android.R.color.tab_indicator_text, null));
         String pattern = "";
         if (dispDate) {
@@ -151,5 +232,34 @@ public class CalcActivity extends AppCompatActivity {
             result.setTextColor(getColor(R.color.calc_error));
         } else
             result.setText(finalDateTime.format(DateTimeFormatter.ofPattern(pattern.trim())));
+    }
+
+    /**
+     * Function bound to button
+     */
+    private void calcResult() {
+        // hide keyboard if open
+        hideKeyboard(textDelta.getWindowToken());
+
+        // By default, display date without year and time without seconds
+        dispDate = true;
+        dispYear = false;
+        dispTime = true;
+        dispSeconds = false;
+
+        // base date detection
+        LocalDate date = detectDate(textDate.getText().toString());
+        // base time detection
+        LocalTime time = detectTime(textTime.getText().toString());
+
+        // combine date and time into a single variable
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+
+        TimeSpan delta = detectDelta(textDelta.getText().toString());
+
+        LocalDateTime finalDateTime = processDelta(dateTime, delta);
+
+        // render with correct format
+        renderResult(finalDateTime);
     }
 }
---
+++ app/src/main/java/com/larsluph/timeutils/calc/TimeSpan.java
@@ -0,0 +1,15 @@
+package com.larsluph.timeutils.calc;
+
+public class TimeSpan {
+    public int days;
+    public int hours;
+    public int minutes;
+    public int seconds;
+
+    public TimeSpan(int d, int h, int m, int s) {
+        days = d;
+        hours = h;
+        minutes = m;
+        seconds = s;
+    }
+}
